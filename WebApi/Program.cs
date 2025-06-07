fb306b4b74fff53957afacdf0d60f4c7d1ccbc88
Amanda Hult <amandahult95@gmail.com>
2025-06-07 15:24:07 +0200

    .

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -44,18 +44,19 @@ builder.Services.AddAuthorization();
 
 builder.Services.AddCors(x =>
 {
-    x.AddPolicy("AllowAll", x =>
+    x.AddPolicy("AllowFrontend", x =>
     {
-        x.AllowAnyMethod();
-        x.AllowAnyOrigin();
-        x.AllowAnyHeader();
+        x.WithOrigins("https://https://icy-pebble-08fddfa03.6.azurestaticapps.net")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
     });
 });
 
 var app = builder.Build();
 app.MapOpenApi();
 app.UseHttpsRedirection();
-app.UseCors("AllowAll");
+app.UseCors("AllowFrontend");
 
 app.UseAuthentication();
 app.UseAuthorization();
